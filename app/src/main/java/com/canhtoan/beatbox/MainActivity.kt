c89f9f8320548bf496adae3fc55815721492c376
nguyen canh toan <v.toannc@vinid.net>
2020-03-01 20:16:08 +0700

    add source

---
+++ app/src/main/java/com/canhtoan/beatbox/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.canhtoan.beatbox
+
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.canhtoan.beatbox.databinding.ActivityMainBinding
+import com.canhtoan.beatbox.databinding.ListItemSoundBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var beatBox: BeatBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        beatBox = BeatBox(assets)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = SoundAdapter(beatBox.sounds)
+        }
+    }
+
+    private inner class SoundHolder(private val binding: ListItemSoundBinding) :
+        RecyclerView.ViewHolder(binding.root) {}
+
+    private inner class SoundAdapter(private val sounds: List<Sound>) : RecyclerView.Adapter<SoundHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                layoutInflater,
+                R.layout.list_item_sound,
+                parent,
+                false
+            )
+
+            return SoundHolder(binding)
+        }
+
+        override fun getItemCount() = sounds.size
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+
+        }
+
+    }
+}
